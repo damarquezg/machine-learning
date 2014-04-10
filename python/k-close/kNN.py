803539e380a06d1b8c594501913d402946ee50c5
yangyimeng <yangbeautydream@gmail.com>
2014-04-10 16:19:30 +0800

    do modify to kNN code

--- python/k-close/kNN.py
+++ python/k-close/kNN.py
@@ -8,6 +8,26 @@ def createDataSet():
     return group,labels
 
 
+def classify0(inX, dataSet, labels, k):
+    import pdb
+    pdb.set_trace()
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndices = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndices[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0];
+
+
+
+
 
 print "hello world, this is the first python machine learning example \n"
 
